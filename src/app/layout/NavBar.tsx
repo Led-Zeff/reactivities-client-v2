51faaf6e21010b9cf7c548ef7b6ea9162d768d5e
Led-Zeff <ortega.victor.t.v@gmail.com>
2021-02-22 19:26:22 -0600

    Activities components

--- src/app/layout/NavBar.tsx
+++ src/app/layout/NavBar.tsx
@@ -1,10 +1,11 @@
-import { Button, Container, Menu } from 'semantic-ui-react';
+import { Button, Container, Icon, Menu } from 'semantic-ui-react';
 
 export default function NavBar() {
   return (
     <Menu inverted fixed="top">
       <Container>
         <Menu.Item header>
+          <Icon name="react" />
           Reactivities
         </Menu.Item>
         <Menu.Item name="Activities" />
