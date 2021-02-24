e18bd3e5dd1d32fc68bffbbfe2e62cc3d5f0777e
Led-Zeff <ortega.victor.t.v@gmail.com>
2021-02-23 20:53:34 -0600

    Edit form

--- src/features/activities/dashboard/ActivityList.tsx
+++ src/features/activities/dashboard/ActivityList.tsx
@@ -3,9 +3,10 @@ import { Activity } from "../../../app/models/activity";
 
 interface Props {
   activities: Activity[];
+  selectActivity: (id: string) => void;
 }
 
-export default function ActivityList({activities}: Props) {
+export default function ActivityList({activities, selectActivity}: Props) {
   return (
     <Segment>
       <Item.Group divided>
@@ -19,8 +20,8 @@ export default function ActivityList({activities}: Props) {
                 <div>{activity.city}, {activity.venue}</div>
               </Item.Description>
               <Item.Extra>
-                <Button floated="right" content="View" color="blue" />
                 <Label basic content={activity.category} />
+                <Button floated="right" content="View" color="blue" onClick={() => selectActivity(activity.id)} />
               </Item.Extra>
             </Item.Content>
           </Item>
--- src/features/activities/details/ActivityDetails.tsx
+++ src/features/activities/details/ActivityDetails.tsx
@@ -1,11 +1,14 @@
+import { Fragment } from 'react';
 import { Button, Card, Image } from 'semantic-ui-react';
 import { Activity } from '../../../app/models/activity';
 
 interface Props {
   activity: Activity;
+  cancelSelectActivity: () => void;
+  openForm: (id: string) => void;
 }
 
-export default function ActivityDetails({activity}: Props) {
+export default function ActivityDetails({activity, cancelSelectActivity, openForm}: Props) {
   return (
     <Card fluid>
       <Image src="/assets/activities.png" />
@@ -19,8 +22,8 @@ export default function ActivityDetails({activity}: Props) {
 
       <Card.Content extra>
         <Button.Group widths="2">
-          <Button basic color="blue" content="Edit" />
-          <Button basic color="grey" content="Cancel" />
+          <Button basic color="blue" content="Edit" onClick={() => openForm(activity.id)} />
+          <Button basic color="grey" content="Cancel" onClick={cancelSelectActivity} />
         </Button.Group>
       </Card.Content>
     </Card>
